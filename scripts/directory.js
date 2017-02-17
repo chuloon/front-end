20601b6ebe9fa72ad7f36d653a8731995518f7f3
chuloon <ben.ng@outlook.com>
2017-02-16 21:19:27 -0500

    selected rows

--- scripts/directory.js
+++ scripts/directory.js
@@ -1,8 +1,50 @@
 $(document).ready(function () {
     ko.applyBindings(new directoryViewModel());
 
+    $('#provider-table').DataTable({
+        "bPaginate": false,
+        "sScrollY": 200
+    });
 
+    $('#provider-table tr').click(function () {
+        selectItem(this);
+        $(this).toggleClass('selected-row');
+    });
 });
+
+function selectedObject() {
+    last_name = "",
+    first_name = "",
+    email_address = "",
+    specialty = "",
+    practice_name = ""
+};
+
+selectedItems = [];
+
+selectItem = (data) => {
+    let retData;
+    if ($(data).length > 0) {
+        retData = $(data)[0].innerText.split('\n')
+    }
+
+    let retObject = new selectedObject();
+    retObject.last_name = retData[0].split(', ')[0];
+    retObject.first_name = retData[0].split(', ')[1];
+    retObject.email_address = retData[1].trim();
+    retObject.specialty = retData[2];
+    retObject.practice_name = retData[3];
+
+    if (!$(data).hasClass('selected-row'))
+        selectedItems.push(retObject);
+    else {
+        $.each(selectedItems, (index, item) => {
+            if (item.email_address == retObject.email_address) {
+                selectedItems.splice(index, 1);
+            }
+        })
+    }
+}
 
 function tog(v) { return v ? 'addClass' : 'removeClass'; }
 $(document).on('input', '.clearable', function () {
@@ -22,25 +64,8 @@ function directoryViewModel() {
     { "last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry" },
     { "last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters" },
     { "last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics" },
-    { "last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics" },
-    { "last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice" },
-
-    { "last_name": "Harris", "first_name": "Mike", "email_address": "mharris@updox.com", "specialty": "Pediatrics", "practice_name": "Harris Pediatrics" },
-    { "last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry" },
-    { "last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters" },
-    { "last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics" },
-    { "last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics" },
-    { "last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice" }, { "last_name": "Harris", "first_name": "Mike", "email_address": "mharris@updox.com", "specialty": "Pediatrics", "practice_name": "Harris Pediatrics" },
-    { "last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry" },
-    { "last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters" },
-    { "last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics" },
-    { "last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics" },
-    { "last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice" }, { "last_name": "Harris", "first_name": "Mike", "email_address": "mharris@updox.com", "specialty": "Pediatrics", "practice_name": "Harris Pediatrics" },
-    { "last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry" },
-    { "last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters" },
-    { "last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics" },
-    { "last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics" },
-    { "last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice" },
+    { "last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting's Well Kids Pediatrics" },
+    { "last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice" }
     ];
 
     this.inputFields = [
